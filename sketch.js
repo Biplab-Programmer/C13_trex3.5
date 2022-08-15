020a4e9a70a1d81bf924caf23f73d6f9a5915277
Biplab-Programmer <111139126+Biplab-Programmer@users.noreply.github.com>
2022-08-15 17:57:54 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -11,10 +11,10 @@ function preload(){
   trex_running = loadAnimation("trex1.png","trex3.png","trex4.png");
   trex_collided = loadAnimation("trex_collided.png");
   
-  groundImage = loadImage("ground2.png");
+  groundImage = lssoadImage("ground2.png");
   
-  cloudImage = loadImage("cloud.png");
- 
+  cloudImage = loadImage("scloud.png");
+ ss
 }
 
 function setup() {
